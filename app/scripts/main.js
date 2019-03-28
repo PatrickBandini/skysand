7a93ca2219713217279163e3144a76e043709f3f
Patrick Bandini <pbandini@vista.it>
2019-03-28 17:32:24 +0100

    homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,48 @@
-console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+    $('#offerte-owl').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText: ['<i class="mdi mdi-arrow-left" aria-hidden="true"></i>','<i class="mdi mdi-arrow-right" aria-hidden="true"></i>'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    })
+
+    var slider = $('#main-slide').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:true,
+      navText: ['',''],
+      dots:false,
+      // autoplay:true,
+      // autoplayTimeout:5000,
+      // autoplayHoverPause:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          992:{
+              items:1
+          }
+      }
+    })
+
+    $('#main-slide-prev').click(function(){
+      slider.trigger('prev.owl.carousel')
+    })
+
+    $('#main-slide-next').click(function(){
+      slider.trigger('next.owl.carousel')
+    })
+})
+
 
 // Uncomment to enable Bootstrap tooltips
 // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
